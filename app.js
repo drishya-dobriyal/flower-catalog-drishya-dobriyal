d6200393067cc406ab3c9d8126cb94eb1a6f089e
Drishya Dobriyal <dobriyaldrishya@gmail.com>
2020-01-29 23:09:41 +0530

    modified server

---
+++ app.js
@@ -0,0 +1,29 @@
+const matchRoute = function (route, request) {
+  if (route.method) {
+    return route.method === request.method && request.url.match(route.path);
+  }
+  return true;
+}
+
+class App {
+  constructor() {
+    this.routes = [];
+  }
+  get(path, handler) {
+    this.routes.push({ path, handler, method: 'GET' });
+  }
+  post(path, handler) {
+    this.routes.push({ path, handler, method: 'POST' });
+  }
+  serve(request, response) {
+    const matchingHandlers = this.routes.filter((route) => matchRoute(route, request));
+    const next = function () {
+      if (matchingHandlers.length === 0) return;
+      const router = matchingHandlers.shift();
+      router.handler(request, response, next);
+    };
+    next();
+  };
+};
+
+module.exports = { App }
--- httpServer.js
+++ httpServer.js
@@ -1,22 +1,16 @@
 const http = require('http');
-const { serveStaticPage, serveHomePage } = require('./lib/serveStaticPage');
-const { serveGuestPageComment, serveGuestPage } = require('./lib/serveGuestPages');
 
-const findHandler = function (request) {
-  if (request.method === 'GET' && request.url === '/guestBooks.html') return serveGuestPage;
-  if (request.method === 'POST' && request.url === '/guestBooks.html') return serveGuestPageComment;
-  if (request.method === 'GET' && request.url === '/') return serveHomePage;
-  if (request.method === 'GET') return serveStaticPage;
-};
+const { App } = require('./app');
+const { serveStaticPage, serveHomePage, serveGuestPageComment, serveGuestPage, pageNotFound } = require('./lib/handlers');
 
-const handleConnection = (request, response) => {
-  const handler = findHandler(request);
-  handler(request, response);
-};
+const app = new App();
 
-const main = function () {
-  const server = new http.Server(handleConnection);
-  server.listen(4000);
-};
+app.get('', serveStaticPage);
+app.get('/guestBooks.html', serveGuestPage);
+app.post('/guestBooks.html', serveGuestPageComment);
+app.get('', pageNotFound);
+app.post('', pageNotFound);
 
-main();
+const server = new http.Server(app.serve.bind(app));
+
+server.listen(4000);
