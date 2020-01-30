e5428f44732cb9da5323bf36b41ee64b483fa354
Drishya Dobriyal <dobriyaldrishya@gmail.com>
2020-01-30 18:16:58 +0530

    modified app.js

--- httpServer.js
+++ httpServer.js
@@ -1,20 +1,6 @@
 const http = require('http');
 
-const { App } = require('./lib/app');
-const {
-  serveStaticPage,
-  serveGuestPageComment,
-  serveGuestPage,
-  pageNotFound
-} = require('./lib/handlers');
-
-const app = new App();
-
-app.get('', serveStaticPage);
-app.get('/guestBooks.html', serveGuestPage);
-app.post('/guestBooks.html', serveGuestPageComment);
-app.get('', pageNotFound);
-app.post('', pageNotFound);
+const { app } = require('./lib/assignHandlers');
 
 const server = new http.Server(app.serve.bind(app));
 
